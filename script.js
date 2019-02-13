53df4a2e8d963e276342ea0d2f4e44a8cf09a8d7
SiewLeng <siewleng.lim@gmail.com>
2019-02-13 22:05:01 +0800

    MVC

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-var monkey = "hello";
+'./tweets';
+
+class List extends React.Component {
+    render() {
+        let itemsElements = this.props.items.map(item => {
+            let str = item.created_at;
+            let url = item.user.profile_image_url;
+            return (
+                <tr>
+                    <td>
+                        <img src={url}/>
+                    </td>
+                    <td>
+                        <p> {item.user.screen_name + " " + str.substring(0, str.length-10)} </p>
+                        <p> {item.text} </p>
+                    </td>
+                </tr>
+            )
+        });
+
+        return (
+            <div>
+                <h3> Tweets </h3>
+                <table>
+                    {itemsElements}
+                </table>
+            </div>
+        );
+    }
+}
+
+
 ReactDOM.render(
-    <div>
-      {monkey}
-    </div>,
+    <List items={tweets} />,
     document.getElementById('root')
 );
